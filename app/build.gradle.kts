59412f8f28ee478127e6d5d631d62b0a7a5453aa
SeSaM <hussamselmy@gmail.com>
2026-09-06 15:20:43 +0300

    Version 1.7.1(3.3.1): Release version code update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ android {
         minSdk = 16
         targetSdk = 28
         versionCode = 107
-        versionName = "1.7.0(3.3.1)"
+        versionName = "1.7.1(3.3.1)"
         setProperty("archivesBaseName", "Emzoom AA_v${versionName}")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
